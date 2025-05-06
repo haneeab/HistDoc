413cfaea256eaaa1381c4f3f2673f024ddb7d87b
einasns <einas.mansor@gmail.com>
2025-05-06 22:47:40 +0300

    finalss

--- FinalProject/frontend/src/components/App.js
+++ FinalProject/frontend/src/components/App.js
@@ -8,10 +8,19 @@ import DeveloperModelsPage from "./DeveloperModelsPage"
 import Register from "./Register";
 import UserImages from "./UserImages";
 import AboutUs from "./AboutUs";
+import AnnotateModelEditor from "./AnnotateModelEditor"
 import FeedbackPage from "./FeedbackPage"
 import DeveloperModelFeedback from "./DeveloperModelFeedback"
 import AllModelsFeedbackSummary from "./AllModelsFeedbackSummary"
 import SortedModelsPage from "./SortedModelsPage"
+import ManuscriptListPage from "./ManuscriptListPage"
+import ManuscriptFilesPage from "./ManuscriptFilesPage"
+import GroundFoldersPage from "./GroundFoldersPage"
+import GroundXMLFilesPage from "./GroundXMLFilesPage"
+import XMLAnnotatorPage  from "./XMLAnnotatorPage"
+import ModelSelectionPage from "./ModelSelectionPage"
+import DeveloperModelParameters from "./DeveloperModelParameters"
+import DeveloperTestModelPage from "./DeveloperTestModelPage"
 function App() {
   return (
     <Router>
@@ -19,22 +28,32 @@ function App() {
         <Route path="/AllModelsFeedbackSummary" component={AllModelsFeedbackSummary} />
         <Route path="/LogIn" component={LogIn} />
         <Route path="/SortedModelsPage" component={SortedModelsPage} />
+        <Route exact path="/ManuscriptListPage" component={ManuscriptListPage} />
 
-        <Route path="/feedback/:extraction_id" component={FeedbackPage} />
+        <Route path="/feedback/:modelId" component={FeedbackPage} />
         <Route path="/about-us" component={AboutUs} />
         <Route path="/user-images" component={UserImages} />
         <Route path="/developer-feedbacks" component={DeveloperModelFeedback} />
+        <Route path="/ManuscriptFilesPage" component={ManuscriptFilesPage} />
+        <Route path="/select-model" component={ModelSelectionPage} />
+<Route path="/annotate-model/:modelId" component={AnnotateModelEditor} />
+        <Route path="/developer-test-model" component={DeveloperTestModelPage} />
 
         <Route path="/Register" component={Register} />
         <Route path="/homepage-user" component={HomepageUser} />
         <Route path="/developer-models" component={DeveloperModelsPage} />
         <Route path="/researcher-homepage" component={HomepageResearcher} />
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/GroundFoldersPage" component={GroundFoldersPage} />
+                        <Route exact path="/" component={HomePage} />
+                <Route exact path="/XMLAnnotatorPage" component={XMLAnnotatorPage} />
+<Route exact path="/developer-model-parameters/:id" component={DeveloperModelParameters} />
+
+                <Route exact path="/GroundXMLFilesPage" component={GroundXMLFilesPage} />
       </Switch>
     </Router>
   );
-}
-
+}//
+// ManuscriptFilesPage
 export default App;
 import ReactDOM from "react-dom";
 
